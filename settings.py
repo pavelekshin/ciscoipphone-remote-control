2104e4afd26961e6dfad59d103c87b7a72f0b602
Pavel Ekshin <pavel.ekshine@gmail.com>
2024-03-07 16:56:34 +0300

    fix description

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 USER = "phonecontrol"  # CUCM End User username
 USER_PWD = "phonecontrol"  # CUCM End User password
 PAUSE = 1.5  # Pause in seconds between sending keypress
-IP_PATTERN = r"(\d+.\d+.\d+.\d+)"  # regex IP pattern
+IP_PATTERN = r"(\d+.\d+.\d+.\d+)"  # regex IP address pattern
